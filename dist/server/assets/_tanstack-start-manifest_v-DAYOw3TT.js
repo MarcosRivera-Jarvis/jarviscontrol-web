720d5edd7074b506332274bda4e14837aa0a05d3
MarcosRivera-Jarvis <5gmshjrzd4@privaterelay.appleid.com>
2026-06-08 17:51:49 +0200

    Delete dist/server directory

--- dist/server/assets/_tanstack-start-manifest_v-DAYOw3TT.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "/Users/marcotas/Downloads/jarvis-control-source/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BHtpA0yy.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-BHtpA0yy.js" } }] }, "/": { filePath: "/Users/marcotas/Downloads/jarvis-control-source/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CbbTQDSW.js"] } } });
-export {
-  tsrStartManifest
-};
